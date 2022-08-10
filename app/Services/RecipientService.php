0bcb4f657cea942ca8164f251248eb1ffc04ff55
Eray Izgi <erayizgi@gmail.com>
2022-08-10 22:27:49 +0200

    Changed validationRules property of RecipientService to const so it can be accessed statically outside the class and removed redundant methods

--- app/Services/RecipientService.php
+++ app/Services/RecipientService.php
@@ -8,13 +8,11 @@ use Illuminate\Support\Facades\Validator;
 
 class RecipientService
 {
-    private array $validationRules = [
+    public array $validationRules = [
         'email' => ['required', 'email', 'unique:recipients', 'max:255'],
         'first_name' => ['required', 'max:255'],
         'last_name' => ['required', 'max:255']
     ];
-
-    private array $validated;
 
     public function __construct(protected RecipientRepository $recipientRepository)
     {
@@ -28,38 +26,21 @@ class RecipientService
      */
     public function create(array $data): Recipient
     {
-        $this->validate($data);
-        $recipient = $this->recipientRepository->hydrate($this->getValidated());
+        $validated = $this->validate($data);
+        $recipient = $this->recipientRepository->hydrate($validated);
         return $this->recipientRepository->save($recipient);
     }
 
     /**
-     * @return array
-     */
-    private function getValidated(): array
-    {
-        return $this->validated;
-    }
-
-    /**
-     * @param array $validated
-     */
-    private function setValidated(array $validated): void
-    {
-        $this->validated = $validated;
-    }
-
-    /**
      * @param array $data
-     * @return \App\Services\RecipientService
+     * @return array
      * @throws \Illuminate\Validation\ValidationException
      */
-    protected function validate(array $data): self
+    protected function validate(array $data): array
     {
         $validator = Validator::make($data, $this->validationRules);
         $validator->validate();
-        $this->setValidated($validator->validated());
-        return $this;
+        return $validator->validated();
     }
 
 }
